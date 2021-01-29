986e2d76698449fa138228b43c86fd4b985b49cd
Anindha <78213613+DebNath-Anindha@users.noreply.github.com>
2021-01-29 19:54:06 +0600

    Add files via upload

---
+++ Exercise 3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    cin >> a ;
+    cout << "The length of the txt string is: " << a.length();
+    return 0;
+}
---
+++ Exercise 4/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    string b;
+
+    cin>>a;
+    cin>>b;
+    string sum = a + b ;
+
+    cout<< "Display : " +sum;
+
+    return 0;
+}
